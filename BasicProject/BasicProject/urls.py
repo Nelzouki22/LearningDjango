a5fc6f156f6bb2a842985752c8e10dbdeacbb048
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-29 23:18:27 -0400

    Add project files.

---
+++ BasicProject/BasicProject/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, re_path
+import HelloDjangoApp.views
+
+# Django processes URL patterns in the order they appear in the array
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+   re_path(r'^$', HelloDjangoApp.views.index, name='index'),
+   re_path(r'^home$', HelloDjangoApp.views.index, name='home'),
+   re_path(r'^about$', HelloDjangoApp.views.about, name='about')
+ ]
